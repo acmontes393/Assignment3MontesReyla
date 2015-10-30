a794128654a37e095d01afec038248196a985575
Ana Cecilia Montes <ac.montes393@gmail.com>
2015-10-30 16:02:04 +0100

    LOKf

--- MontesReylaEmigration.R
+++ MontesReylaEmigration.R
@@ -1,21 +1,39 @@
 # Collaborative Data Analysis Project 
-# Steps
-# 1. Load and data cleaning
 
+
+# Installing packages
+install.packages('xlsx')
+install.packages('WDI')
+
+#Setting directory
+setwd('/Users/AnaCe/Dropbox/Hertie/CollaborativeDataAnalysis/R/Assignment3MontesReyla')
+
+
+# 1. Load and data cleaning
 # Make an objet
 URL <- "http://esa.un.org/unmigration/TIMSO2013/data/subsheets/UN_MigrantStockByOriginAndDestination_2013T10.xls"
 #Create a temporary file to put the zip 
 temp <- tempfile()
 # Download the compressed 
-download.file(URL, temp)
+download.file(URL, Migration90.xls)
 # 
-OECDMigration2010 <-read.ftable(temp, "DIOC_2010_11_File_A_bis_REV.csv"))
+Migration90 = read.table( "UN_MigrantStockByOriginAndDestination_2013T10.xls")
 
-# put in the code the URL. and then download. 
+# another way
+
+require(xlsx)
+library(rio)
+full <- read.xlsx(temp, sheetIndex = 2)
+
+full <- import("UN_MigrantStockByOriginAndDestination_2013T10.xls", format = "xls", sheet = 2)
+
+full <- readxl::read_excel(temp, sheet = 2)
+
+full <- import(temp, sheet = 2, format = "xls")
 
 # WDI
-install.packages('WDI')
 library(WDI)
 
 WDI_indi<- WDI(country = "all", indicator = c("IT.CEL.SETS.P2", "IT.NET.USER.P2"),
                    start = 1990, end = 2013, extra = FALSE, cache = NULL)
+
